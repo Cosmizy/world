3b079cfa09ace943b3c101ad88bfc6263fa9bd3d
Cosmizy <116780823+Cosmizy@users.noreply.github.com>
2024-02-13 14:16:38 -0800

    Update execute_script2.js

--- execute_script2.js
+++ execute_script2.js
@@ -1,6 +1,4 @@
 /// execute_script2.js
 window.onload = function() {
-    if (window.location.href.indexOf("google.com") !== -1) {
-        document.body.style.transform = "rotate(180deg)";
-    }
+    document.body.style.transform = "rotate(180deg)";
 }
