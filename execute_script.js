3b4deeffd2562bf25f7cf77f48a03c78c31c6d6a
Cosmizy <116780823+Cosmizy@users.noreply.github.com>
2024-02-13 14:04:21 -0800

    Delete execute_script.js

--- execute_script.js
+++
@@ -1,7 +0,0 @@
-window.onload = function() {
-    if (window.location.href.indexOf("google.com") !== -1) {
-        console.log("This is a Google page.");
-
-        document.body.style.transform = "rotate(180deg)";
-    }
-}
